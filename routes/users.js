3e696a8e7d03ed6d3741d90ba092317e3648fbbe
ananyagoel2 <goelananya.ag@gmail.com>
2016-12-22 03:15:09 +0530

    added users list route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var user_m= require('../models/model_user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    user_m.find(function (err, user) {
+        if (err) return next(err);
+        res.json(user);
+    });
 });
+
+
 
 module.exports = router;
